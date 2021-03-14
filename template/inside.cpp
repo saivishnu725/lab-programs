02e5174cd1e9a00b94e2685f8586c91c67f4c5db
saivishnu725 <saivsihnu725@gmail.com>
2021-03-14 18:09:06 +0530

    Template for inside and outside the class definitions.

---
+++ template/inside.cpp
@@ -0,0 +1,28 @@
+//Inside class def
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class cs
+{
+    int i;
+public:
+    void input()
+    {
+    }
+    void logic()
+    {
+    }
+    void display()
+    {
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    cs bh;
+    bh.input();
+    bh.logic();
+    bh.display();
+    return 0;
+}
---
+++ template/outside.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class cs
+{
+    int i;
+
+public:
+    void input();
+    void logic();
+    void display();
+};
+void cs::input()
+{
+}
+void cs::logic()
+{
+}
+void cs::display()
+{
+}
+
+int main(int argc, char const *argv[])
+{
+    cs bh;
+    bh.input();
+    bh.logic();
+    bh.display();
+    return 0;
+}
